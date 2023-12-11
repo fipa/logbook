ed256820d97457c5a59e23881094b0bbb167af55
Fernando Poblete Arrau <fernandopobletearrau@gmail.com>
2023-12-11 10:54:35 -0300

    Configuracion de cors.

--- api/lib/src/main/java/logbook/controller/NoteController.java
+++ api/lib/src/main/java/logbook/controller/NoteController.java
@@ -3,6 +3,7 @@ package logbook.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import logbook.domain.Note;
 import logbook.service.NoteService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class NoteController {
 
 	@Autowired
