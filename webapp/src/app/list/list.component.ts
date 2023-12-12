f90dfe24fee9c31693a22459e24fae3d5032f18e
Fernando Poblete Arrau <fernandopobletearrau@gmail.com>
2023-12-11 23:41:01 -0300

    Cambiando tipo de dato en componente list.

--- webapp/src/app/list/list.component.ts
+++ webapp/src/app/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { LogbookService } from '../services/logbook.service';
+import { Note } from '../models/note.model';
 
 @Component({
   selector: 'app-list',
@@ -9,7 +10,7 @@ import { LogbookService } from '../services/logbook.service';
 })
 
 export class ListComponent implements OnInit{
-  notes: any;
+  notes: Note[] = [];
 
   constructor(private logbookService: LogbookService) {}
   
